55bd1e3e7427ed7ef049816380ab2737f1eef195
Toderica Matei <namelessman26@gmail.com>
2022-06-04 15:38:03 +0300

    added empty class

---
+++ ImToQuadTree.cpp
@@ -0,0 +1,5 @@
+//
+// Created by perry on 6/4/22.
+//
+
+#include "ImToQuadTree.h"
---
+++ ImToQuadTree.h
@@ -0,0 +1,14 @@
+//
+// Created by perry on 6/4/22.
+//
+
+#ifndef IMCOMP_IMTOQUADTREE_H
+#define IMCOMP_IMTOQUADTREE_H
+
+
+class ImToQuadTree {
+
+};
+
+
+#endif //IMCOMP_IMTOQUADTREE_H
